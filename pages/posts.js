a09f67cb3f435a54c077e7626caca54368c440c1
ricardo <r.alves@resolution.de>
2020-10-27 19:30:08 -0300

    add relevance style

--- pages/posts.js
+++ pages/posts.js
@@ -3,25 +3,33 @@ import useSWR from "swr";
 import styles from '../styles.module.css'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
-export default function Movies(posts = []) {
+const getBGcolor = (relevance) => {
+    if (relevance === 2 || relevance === 1)
+        return styles.urgentbg;
 
-    const { data, error } = useSWR('/api/news', fetcher, { refreshInterval: 1000 });
-    // console.log('data', data);
-    // console.log('error', error);
+    return styles.normalbg;
+}
+
+export default function Posts() {
+
+    const { data, error } = useSWR('/api/news', fetcher, { refreshInterval: 10000 });
 
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
-    // setTimeout(() => {
-        let msg = data[0].message;
+
+    setTimeout(() => {
+        const msg = data[0].message;
         const m = new Notification(msg);
-    // }, 3000)
+    }, 2000);
+    console.log('data', data);
     return (
         <div>
             {
-                data.map(({ id, message, link, create_at, fonte }) => (
-                    <div key={id} className={styles.card}>
-                        <div className={styles.container}>
-                            <h4><b>{fonte} | {formatDistanceToNow(create_at)}</b></h4>
+                data.map(({ id, message, link, create_at, fonte, relevance }) => (
+                    <div key={id} className={`${styles.card}`}>
+                        <div className={`${styles.container}`}>
+                            <h4 className={`${getBGcolor(relevance)}`}><b>{fonte} | {formatDistanceToNow(create_at)}</b>
+                            </h4>
                             <p>{message.split('**')[1]}</p>
                             <p><a href={link} target="_blank">{link} </a></p>
                         </div>
