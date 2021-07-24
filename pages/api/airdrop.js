15e63a44475426af7d89503e4964a79861eae564
ricardo <r.alves@resolution.de>
2021-07-24 14:20:10 -0300

    remove sensitive data from network traffic

--- pages/api/airdrop.js
+++ pages/api/airdrop.js
@@ -8,9 +8,13 @@ export default async (req, res) => {
             .find().toArray();
         const newusers = await db
             .collection("profile")
-            .find({ createdAt: { $gt: 1627128000000 } }).toArray();
+            .find({ createdAt: { $gt: 1627128000000 } }, { email: 1})
+            .toArray();
 
-        res.json({ok: true, subs, newusers });
+        const signups = subs.filter(s => s.subscribed);
+        const denials = subs.filter(s => !s.subscribed && s.popupDisabled);
+
+        res.json({ok: true, signups: signups.length, denials: denials.length, newusers: newusers.length});
     } catch (err) {
         console.log('api erro', err);
         res.json({ok: false});
--- pages/ethernity.js
+++ pages/ethernity.js
@@ -17,17 +17,16 @@ const duration = (d) => {
 }
 export default function Ethernity() {
     const { data } = useSWR('/api/airdrop', fetcher, { refreshInterval: 30000 });
-    const subs = data?.subs || [];
-    const newusers = data?.newusers || [];
-    const signups = subs.filter(s => s.subscribed);
-    const denials = subs.filter(s => !s.subscribed && s.popupDisabled);
+    const newusers = data?.newusers || '...';
+    const signups = data?.signups || '...';
+    const denials = data?.denials || '...';
     return (
         <div style={{ display: 'flex', flexDirection: 'column', fontSize: 24, alignItems: 'center' }}>
             <div style={{fontSize: 26, padding: 10}}><strong>Airdrop numbers</strong></div>
             <div><strong>{duration(new Date().getTime() - start)}</strong></div>
-            <div>New users: <strong>{newusers.length}</strong></div>
-            <div>Signups: <strong>{signups.length}</strong></div>
-            <div>Denials: <strong>{denials.length}</strong></div>
+            <div>New users: <strong>{newusers}</strong></div>
+            <div>Signups: <strong>{signups}</strong></div>
+            <div>Denials: <strong>{denials}</strong></div>
         </div>
     );
 }
