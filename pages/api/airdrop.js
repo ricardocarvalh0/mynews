3102d88dcba8143f2ef07600d65cf36836017772
ricardo <r.alves@resolution.de>
2021-08-04 12:12:36 -0300

    messi announcement

--- pages/api/airdrop.js
+++ pages/api/airdrop.js
@@ -4,17 +4,16 @@ export default async (req, res) => {
     const {db} = await connectToDatabase();
     try {
         const subs = await db
-            .collection("airdropsubscription")
+            .collection("dropsubscription")
             .find().toArray();
         const newusers = await db
             .collection("profile")
-            .find({ createdAt: { $gt: 1627128000000 } }, { email: 1})
+            .find({ createdAt: { $gt: 1628089200000 } }, { email: 1})
             .toArray();
 
-        const signups = subs.filter(s => s.subscribed);
         const denials = subs.filter(s => !s.subscribed && s.popupDisabled);
 
-        res.json({ok: true, signups: signups.length, denials: denials.length, newusers: newusers.length});
+        res.json({ok: true, signups: subs.length, denials: denials.length, newusers: newusers.length});
     } catch (err) {
         console.log('api erro', err);
         res.json({ok: false});
--- pages/ethernity.js
+++ pages/ethernity.js
@@ -7,7 +7,7 @@ const fetcher = (...args) => fetch(...args).then(res => {
     return res.json()
 })
 
-const start = 1627128000000;
+const start = 1628089200000;
 
 const duration = (d) => {
     const hours = d / 1000 / 60 / 60;
@@ -16,18 +16,18 @@ const duration = (d) => {
     return `${intHours}h${minutes}m`
 }
 export default function Ethernity() {
-    // const { data } = useSWR('/api/airdrop', fetcher, { refreshInterval: 30000 });
-    const data = {};
+    const { data } = useSWR('/api/airdrop', fetcher, { refreshInterval: 30000 });
+    // const data = {};
     const newusers = data?.newusers || '...';
     const signups = data?.signups || '...';
     const denials = data?.denials || '...';
     return (
         <div style={{ display: 'flex', flexDirection: 'column', fontSize: 24, alignItems: 'center' }}>
-            <div style={{fontSize: 26, padding: 10}}><strong>Airdrop numbers</strong></div>
+            <div style={{fontSize: 26, padding: 10}}><strong>Messi registration numbers</strong></div>
             <div><strong>{duration(new Date().getTime() - start)}</strong></div>
             <div>New users: <strong>{newusers}</strong></div>
             <div>Signups: <strong>{signups}</strong></div>
-            <div>Denials: <strong>{denials}</strong></div>
+            {/*<div>Denials: <strong>{denials}</strong></div>*/}
         </div>
     );
 }
