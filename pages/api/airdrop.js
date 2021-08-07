6579dda62944df837986e962c322fa103e15e730
ricardo <r.alves@resolution.de>
2021-08-07 17:40:06 -0300

    fix messi numbers

--- pages/api/airdrop.js
+++ pages/api/airdrop.js
@@ -5,15 +5,14 @@ export default async (req, res) => {
     try {
         const subs = await db
             .collection("dropsubscription")
-            .find().toArray();
+            .count();
         const newusers = await db
             .collection("profile")
-            .find({ createdAt: { $gt: 1628089200000 } }, { email: 1})
-            .toArray();
+            .countDocuments({ createdAt: { $gt: 1628089200000 } });
 
-        const denials = subs.filter(s => !s.subscribed && s.popupDisabled);
+        console.log({ subs, newusers})
 
-        res.json({ok: true, signups: subs.length, denials: denials.length, newusers: newusers.length});
+        res.json({ok: true, signups: subs.toLocaleString(), denials: 0, newusers: newusers.toLocaleString()});
     } catch (err) {
         console.log('api erro', err);
         res.json({ok: false});
